c095c4b636936258dd2de5828e7ebf5e8ea966a2
Ken Ruf <ruf.kenichi@gmail.com>
2019-08-04 21:10:57 -0500

    finished projectRouter middleware

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -41,4 +41,46 @@ router.post('/', async (req, res)  => {
   }
 });
 
+async function validateProjectId(req, res, next) {
+  try{
+    const { id } = req.params;
+    const project = await Project.get(id);
+    if(project) {
+      req.project = project;
+      next();
+    } else {
+      res.status(404).json({ message: "invalid project id" });
+    }
+  }
+  catch(error) {
+    res.status(500).json(error);
+  }
+};
+
+function validateProjectBody(req, res, next) {
+  try{
+    const body = req.body;
+    if(!body) {
+      res.status(400).json({ message: "missing request body" });
+    } else {
+      next();
+    }
+  }
+}
+
+function validateAction(req, res, next) {
+  try{
+    const body = req.body;
+    if(!body) {
+      res.status(400).json({ message: "missing request body"});
+    } else if(!body.name) {
+      res.status(400).json({ message: "project name is required" });
+    } else if(!body.description) {
+      res.status(400).json({ message: "project description is required" });
+    } else {
+      next();
+    }
+  }
+}
+
 module.exports = router;
