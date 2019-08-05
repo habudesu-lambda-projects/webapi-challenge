ff984822c7de3a1a83e1367043b927e76b07c97f
Ken Ruf <ruf.kenichi@gmail.com>
2019-08-04 22:30:07 -0500

    everything is working except for the error case when action id is invalid

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -5,44 +5,67 @@ const Action = require('../helpers/actionModel');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try{
+router.get('/:id', validateProjectId, (req, res) => {
+  res.status(200).json(req.project);
+});
 
+router.post('/', validateProject, async (req, res)  => {
+  try {
+    const project = await Project.insert(req.body);
+    res.status(201).json(project);
   }
-  catch{
-
+  catch(error) {
+    res.status(500).json(error);
   }
 });
 
-router.get('/:id', async (req, res)  => {
-  try{
-
+router.put('/:id', validateProjectId, validateProject, async (req, res)  => {
+  const { id } = req.params;
+  const body = req.body;
+  try {
+    const updatedProject = await Project.update(id, body);
+    res.status(201).json({ message: "Project Data Updated", project: updatedProject});
   }
-  catch{
-
-  }
-});
-
-router.post('/', async (req, res)  => {
-  try{
-
-  }
-  catch{
-
+  catch(error) {
+    res.status(500).json(error);
   }
 });
 
-router.post('/', async (req, res)  => {
-  try{
-
+router.delete('/:id', validateProjectId, async (req, res)  => {
+  const { id } = request.params;
+  try {
+    const deleted = await Project.remove(id);
+    res.status(200).json({ message: "Project Deleted", project: req.project})
   }
-  catch{
-
+  catch(error) {
+    res.status(500).json(error);
   }
 });
+
+router.get('/:id/actions', validateProjectId, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const actions = await Project.getProjectActions(id);
+    res.status(200).json(actions);
+  } catch(error) {
+    res.status(500).json(error);
+  }
+})
+
+router.post('/:id/actions', validateProjectId, validateAction, async (req, res) => {
+  const body = req.body;
+  try {
+    const addedAction = await Action.insert(body);
+    res.status(201).json({ message: "Action Added", action: addedAction})
+  } catch(error) {
+    res.status(500).json(error);
+  }
+})
+
+// validatio middlewares
 
 async function validateProjectId(req, res, next) {
-  try{
+  try {
     const { id } = req.params;
     const project = await Project.get(id);
     if(project) {
@@ -57,19 +80,8 @@ async function validateProjectId(req, res, next) {
   }
 };
 
-function validateProjectBody(req, res, next) {
-  try{
-    const body = req.body;
-    if(!body) {
-      res.status(400).json({ message: "missing request body" });
-    } else {
-      next();
-    }
-  }
-}
-
-function validateAction(req, res, next) {
-  try{
+function validateProject(req, res, next) {
+  try {
     const body = req.body;
     if(!body) {
       res.status(400).json({ message: "missing request body"});
@@ -80,6 +92,27 @@ function validateAction(req, res, next) {
     } else {
       next();
     }
+  } catch(error) {
+    res.status(500).json(error);
+  }
+}
+
+function validateAction(req, res, next) {
+  try {
+    const body = req.body;
+    if(!body) {
+      res.status(400).json({ message: "missing request body"});
+    } else if(!body.description) {
+      res.status(400).json({ message: "action description is required" });
+    } else if(!body.notes) {
+      res.status(400).json({ message: "action notes is required" });
+    } else if(!body.project_id) {
+      res.status(400).json({ message: "action project_id is required" });
+    } else {
+      next();
+    }
+  } catch(error) {
+    res.status(500).json(error);
   }
 }
 
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const projectRouter = require('./data/routers/projectRouter');
 const server = express();
 
 server.use(express.json());
-server.use('/api/actions', actionRouter);
 server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.get('/', ( req, res ) => {
   res.status(200).send('<h1>API for WebAPI Sprint Challenge</h1>')
