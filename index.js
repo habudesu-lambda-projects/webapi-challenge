c311d659ced51596fb07b7f5f1213ee39a76b33a
Ken Ruf <ruf.kenichi@gmail.com>
2019-08-04 20:49:25 -0500

    installed dependencies, setup index.js, created server.js, actionRouter.js, and projectRouter.js

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const server = require('./server.js');
+
+const port = process.env.PORT ? process.env.PORT : 4000;
+
+server.listen(port, () => {
+  console.log(`Server running on http:localhost:${port}`);
+});
